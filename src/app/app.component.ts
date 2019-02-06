f01ba035f11ad549d93e90d41b2ad28886dcc8bd
Ahmad Bayat <ahmad.bayat@vectrus.com>
2019-02-06 10:54:22 -0500

    latest code to repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chatapp';
+  items: FirebaseListObservable<any[]>;
+  msg: string = '';
+  editMsg: boolean = false;
+  editId: number;
+
+  constructor(public af: AngularFireDatabase){
+    this.items = af.list('/messages');
+  }
+
+  send(chatMsg: string){
+    this.items.push({message: chatMsg});
+    this.msg = '';
+  }
+
+  delete(key: string){
+    this.items.remove(key);
+  }
+
+  edit(key:string, message: string){
+    this.items.update(key, {message:message});
+    this.editMsg = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBv7BcCLSsaerzJ1er-HdtoiEz2NRzIdLQ",
+  authDomain: "chatapp-4c4ba.firebaseapp.com",
+  databaseURL: "https://chatapp-4c4ba.firebaseio.com",
+  projectId: "chatapp-4c4ba",
+  storageBucket: "chatapp-4c4ba.appspot.com",
+  messagingSenderId: "801923220093"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
